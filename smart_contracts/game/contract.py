08c5060d7dffa0a0c970d464aec92f0df0879b8a
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2025-05-04 03:39:35 +0530

    Create GameState Structure

--- smart_contracts/game/contract.py
+++ smart_contracts/game/contract.py
@@ -10,3 +10,10 @@ Board: TypeAlias = arc4.StaticArray[arc4.Byte, Literal[9]]
 # Add game constants
 HOST_MARK = 1
 GUEST_MARK = 2
+
+class GameState(arc4.Struct, kw_only=True):
+    board: Board
+    host: arc4.Address
+    guest: arc4.Address
+    is_over: arc4.Bool
+    turns: arc4.UInt8
