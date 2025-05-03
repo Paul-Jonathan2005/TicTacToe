1b4020089ba99ca5882e70c9f21e4085f92fc461
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2025-05-04 03:43:10 +0530

    Initialize Contract Class

--- smart_contracts/game/contract.py
+++ smart_contracts/game/contract.py
@@ -4,12 +4,18 @@
 from typing import Literal, TypeAlias
 from algopy import (
     ARC4Contract,
-    arc4,)
+    arc4,
+    BoxMap,
+    LocalState,
+    UInt64,
+)
+
 Board: TypeAlias = arc4.StaticArray[arc4.Byte, Literal[9]]
 
 # Add game constants
 HOST_MARK = 1
 GUEST_MARK = 2
+
 
 class GameState(arc4.Struct, kw_only=True):
     board: Board
@@ -17,3 +23,13 @@ class GameState(arc4.Struct, kw_only=True):
     guest: arc4.Address
     is_over: arc4.Bool
     turns: arc4.UInt8
+
+
+class TicTacToe(ARC4Contract):
+    def __init__(self) -> None:
+        self.id_counter = UInt64(0)
+
+        self.games_played = LocalState(UInt64)
+        self.games_won = LocalState(UInt64)
+
+        self.games = BoxMap(UInt64, GameState)
