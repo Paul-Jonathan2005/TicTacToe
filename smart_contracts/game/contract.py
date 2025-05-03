8e6529578a095fb5ef2d23d8541aefe7a51f7a0b
Paul-Jonathan2005 <kakani.jonathan@gmail.com>
2025-05-04 03:38:59 +0530

    Define Game Constants

--- smart_contracts/game/contract.py
+++ smart_contracts/game/contract.py
@@ -4,7 +4,9 @@
 from typing import Literal, TypeAlias
 from algopy import (
     ARC4Contract,
-    arc4,
-)
-
+    arc4,)
 Board: TypeAlias = arc4.StaticArray[arc4.Byte, Literal[9]]
+
+# Add game constants
+HOST_MARK = 1
+GUEST_MARK = 2
