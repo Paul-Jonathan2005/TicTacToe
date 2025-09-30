8c88d668ba64dd42b1713224ffa588003dd50beb
Paul Jonathan Kakani <kakani.jonathan@gmail.com>
2025-09-30 21:07:37 +0530

    Define the GameState struct

--- smart_contracts/game/contract.py
+++ smart_contracts/game/contract.py
@@ -4,3 +4,10 @@ from algopy import ARC4Contract, BoxMap, LocalState, UInt64, arc4
 Board: TypeAlias = arc4.StaticArray[arc4.Byte, Literal[9]]
 HOST_MARK = 1
 GUEST_MARK = 2
+
+class GameState(arc4.Struct, kw_only=True):
+    board: Board
+    host: arc4.Address
+    guest: arc4.Address
+    is_over: arc4.Bool
+    turns: arc4.UInt8
